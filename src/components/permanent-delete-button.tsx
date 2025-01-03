0a11a9126bf59efdddc2935191259b4bc8689c4e
jvstln <173433315+jvstln@users.noreply.github.com>
2025-01-03 14:01:05 +0100

    Fix permanent-delete-button dialog layout shift
    Convert permanent-delete-button from card to dialog

--- src/components/permanent-delete-button.tsx
+++ src/components/permanent-delete-button.tsx
@@ -6,6 +6,15 @@ import { toast } from "sonner";
 import { Button } from "~/components/ui/button";
 import { deleteNote as deleteNoteServer } from "~/server/actions/notes/delete-note";
 import { Card, CardFooter, CardHeader } from "./ui/card";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+} from "./ui/dialog";
+import { DialogDescription, DialogTitle } from "@radix-ui/react-dialog";
 
 const PermanentlyDeleteButton = ({
   noteId,
@@ -30,64 +39,54 @@ const PermanentlyDeleteButton = ({
       });
     });
   };
-  const toggleIsOpen = () => {
-    setIsOpen(!isOpen);
-  };
-  const handleCardClick = (e: React.MouseEvent) => {
-    e.stopPropagation();
-    e.preventDefault();
-  };
+
   return (
-    <>
-      <Button
-        onClick={toggleIsOpen}
-        disabled={isPending}
-        className="rounded-md bg-red-500 px-3 py-1.5 text-sm font-medium text-custom-dark-900 transition-all duration-300 hover:bg-red-400 hover:shadow-lg hover:shadow-custom-yellow-500/20"
-      >
-        {iconOnly ? <Trash /> : "Delete Permanently"}
-      </Button>
-      {isOpen && (
-        <section
-          onClick={toggleIsOpen}
-          className={
-            "absolute left-0 top-0 z-[1002] flex min-h-screen w-full flex-col items-center justify-center bg-[#00000063] backdrop-blur-sm"
-          }
+    <Dialog
+      open={isOpen}
+      onOpenChange={(value) => {
+        if (isPending) return;
+        setIsOpen(value);
+      }}
+    >
+      <DialogTrigger asChild>
+        <Button
+          size="sm"
+          disabled={isPending}
+          className="mt-1 w-full justify-start rounded-md bg-red-500 text-sm font-medium text-custom-dark-900 transition-all duration-300 hover:bg-red-400 hover:shadow-lg hover:shadow-custom-yellow-500/20"
         >
-          <Card
-            onClick={handleCardClick}
-            className="w-[370px] shadow-md sm:w-[420px] md:w-[500px]"
+          {iconOnly ? <Trash /> : "Delete Permanently"}
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="text-3xl font-extrabold text-red-500">
+            Permanently delete note?
+          </DialogTitle>
+          <DialogDescription className="text-center">
+            This action cannot be undone. This will permanently delete this
+            note.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button
+            variant={"ghost"}
+            disabled={isPending}
+            onClick={deleteNote}
+            className="flex-1 rounded-md bg-red-500 px-3 py-1.5 text-sm font-medium text-custom-dark-900 transition-all duration-300 hover:bg-red-400 hover:shadow-lg hover:shadow-custom-yellow-500/20 focus:outline-none"
           >
-            <CardHeader className="flex flex-col items-center justify-center">
-              <h3 className="text-3xl font-extrabold text-red-500">
-                Permanently delete note?
-              </h3>
-              <p className="text-center">
-                This action cannot be undone. This will permanently delete this
-                note.
-              </p>
-            </CardHeader>
-            <CardFooter className="flex justify-center space-x-4">
-              <Button
-                variant={"ghost"}
-                disabled={isPending}
-                onClick={deleteNote}
-                className="flex-1 rounded-md bg-red-500 px-3 py-1.5 text-sm font-medium text-custom-dark-900 transition-all duration-300 hover:bg-red-400 hover:shadow-lg hover:shadow-custom-yellow-500/20 focus:outline-none"
-              >
-                {isPending ? <ClipLoader size={16} color="black" /> : "Delete"}
-              </Button>
-              <Button
-                variant={"ghost"}
-                disabled={isPending}
-                onClick={toggleIsOpen}
-                className="flex-1 rounded-md bg-gray-500 px-3 py-1.5 text-sm font-medium text-white transition-all duration-300 hover:bg-gray-400 hover:shadow-lg hover:shadow-custom-yellow-500/20 focus:outline-none"
-              >
-                Cancel
-              </Button>
-            </CardFooter>
-          </Card>
-        </section>
-      )}
-    </>
+            {isPending ? <ClipLoader size={16} color="black" /> : "Delete"}
+          </Button>
+          <Button
+            disabled={isPending}
+            variant="ghost"
+            className="flex-1 rounded-md bg-gray-500 px-3 py-1.5 text-sm font-medium text-white transition-all duration-300 hover:bg-gray-400 hover:shadow-lg hover:shadow-custom-yellow-500/20 focus:outline-none"
+            asChild
+          >
+            <DialogClose>Cancel</DialogClose>
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
 
